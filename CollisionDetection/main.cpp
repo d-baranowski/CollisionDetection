d2e7b9b6732bece91fd3cdc35dc22f87d4d95e17
Daniel Baranowski <dan@devtales.net>
2018-05-08 20:46:40 +0100

    Simple window in glut

---
+++ CollisionDetection/main.cpp
@@ -0,0 +1,14 @@
+#include "GL\freeglut.h"
+
+int main(int argc, char **argv)
+{
+	// init GLUT and create Window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(320, 320);
+	glutCreateWindow("Lighthouse3D- GLUT Tutorial");
+
+	system("PAUSE");
+	return 1;
+}
